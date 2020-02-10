cf0dd8570d0c9dc544812851a53bdc9376acd714
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-02-11 01:55:35 +0200

    Add rust example

---
+++ Rust/1-struct.rs
@@ -0,0 +1,28 @@
+struct Date {
+    year: u32,
+    month: u32,
+    day: u32,
+}
+
+struct Person {
+    name: String,
+    city: String,
+    born: Date,
+}
+
+fn main() {
+    let p1 = Person {
+        name: String::from("Marcus"),
+        city: String::from("Roma"),
+        born: Date {
+          day: 26,
+          month: 4,
+          year: 121,
+        },
+    };
+
+    println!(
+      "Name: {}\nCity: {}\nBorn: {}-{}-{}\n",
+      p1.name, p1.city, p1.born.year, p1.born.month, p1.born.day
+    );
+}
